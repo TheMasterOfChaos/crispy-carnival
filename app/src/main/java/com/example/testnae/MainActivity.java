672922399a07906c2a20eba4aa4aa9b9b80dd499
jtmoc <bir.german18@gmail.com>
2019-05-01 17:05:02 +0300

    first commit

---
+++ app/src/main/java/com/example/testnae/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.testnae;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkAuthStatus();
+        setContentView(R.layout.activity_main);
+        BottomNavigationView mainNavigationView = findViewById(R.id.main_navigation_view);
+        mainNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                //TODO выбор фрагментов
+                return false;
+            }
+        });
+    }
+
+    private void checkAuthStatus() {
+        //TODO проверка статуса аутинтификации, и запуск активности для аутинтификации
+    }
+}
